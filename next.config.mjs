38d2b50ad07315fc0509c7e377902eeb344d6fa8
akio <124953279+akiomatic@users.noreply.github.com>
2025-05-11 04:39:44 +0900

    fix: envのインポートパスを修正し、src/appからsrcに変更

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { createJiti } from "jiti";
 
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
-await jiti.import("./src/app/env");
+await jiti.import("./src/env");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
