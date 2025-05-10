72b3baf3ae8dd12254d75f3b4ad2387a86d29ad8
akio <124953279+akiomatic@users.noreply.github.com>
2025-05-11 05:32:50 +0900

    feat: 都道府県コードによる人口構成データのフェッチ関数を追加

--- src/actions/api.ts
+++ src/actions/api.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import { env } from "@/env";
-import { prefecturesResponseSchema } from "@/types";
+import {
+	type TPrefCode,
+	populationCompositionPerYearResponseSchema,
+	prefecturesResponseSchema,
+} from "@/types";
 
 const BASE_URL = `${env.YUMEMI_CODECHECK_API_BASE_URL}/api/v1`;
 
@@ -22,4 +26,32 @@ export const getPrefectures = async () => {
 	const prefectures = await res.json();
 
 	return prefecturesResponseSchema.parse(prefectures).result;
+};
+
+export const getPopulationCompositionPerYearByPrefCode = async (
+	prefCode: TPrefCode,
+) => {
+	"use cache";
+
+	const res = await fetch(
+		`${BASE_URL}/population/composition/perYear?prefCode=${prefCode}`,
+		{
+			headers: {
+				"X-API-KEY": env.YUMEMI_CODECHECK_API_KEY,
+				"Content-Type": "application/json",
+			},
+		},
+	);
+
+	if (!res.ok) {
+		throw new Error(
+			`Failed to fetch population composition per year by prefCode: ${prefCode}`,
+		);
+	}
+
+	const populationCompositionPerYear = await res.json();
+
+	return populationCompositionPerYearResponseSchema.parse(
+		populationCompositionPerYear,
+	).result;
 };
