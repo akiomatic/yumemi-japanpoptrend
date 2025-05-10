16aa198ffa747aba637c1f2444075e1eb0c8b64f
akio <124953279+akiomatic@users.noreply.github.com>
2025-05-11 06:51:52 +0900

    feat: Storybookの設定にT3環境変数を追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import { env as t3Env } from "../src/env";
 
 const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -13,5 +14,8 @@ const config: StorybookConfig = {
 		options: {},
 	},
 	staticDirs: ["../public"],
+	env: {
+		...t3Env,
+	},
 };
 export default config;
