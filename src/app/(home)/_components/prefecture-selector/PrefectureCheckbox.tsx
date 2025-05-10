8308805bb02e3c7a280f521f1c766e3d4cad8ec0
akio <124953279+akiomatic@users.noreply.github.com>
2025-05-11 07:30:19 +0900

    feat: 都道府県選択用チェックボックスコンポーネントを追加

---
+++ src/app/(home)/_components/prefecture-selector/PrefectureCheckbox.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Checkbox } from "@/components/checkbox/Checkbox";
+import { populationSearchParams } from "@/lib/search-params";
+import type { TPrefCode, TPrefecture } from "@/types";
+import { useQueryState } from "nuqs";
+
+type TPrefectureCheckboxProps = {
+	prefecture: TPrefecture;
+};
+
+export const PrefectureCheckbox = ({
+	prefecture,
+}: TPrefectureCheckboxProps) => {
+	const [selectedPrefectures, setSelectedPrefectures] = useQueryState(
+		"prefCodes",
+		populationSearchParams.prefCodes,
+	);
+
+	const addPrefecture = (prefCode: TPrefCode) => {
+		setSelectedPrefectures([...selectedPrefectures, prefCode]);
+	};
+
+	const removePrefecture = (prefCode: TPrefCode) => {
+		setSelectedPrefectures(
+			selectedPrefectures.filter((code) => code !== prefCode),
+		);
+	};
+
+	const handlePrefectureChange = (checked: boolean) => {
+		if (checked) {
+			addPrefecture(prefecture.prefCode);
+		} else {
+			removePrefecture(prefecture.prefCode);
+		}
+	};
+
+	const isChecked = selectedPrefectures.includes(prefecture.prefCode);
+	const checkboxId = `prefecture-checkbox-${prefecture.prefCode}`;
+
+	return (
+		<Checkbox
+			checked={isChecked}
+			onChange={handlePrefectureChange}
+			label={prefecture.prefName}
+			id={checkboxId}
+			className="flex justify-center items-center"
+		/>
+	);
+};
