d55e7cd417ec7e4c6c5b753b29a80a75036ca935
akio <124953279+akiomatic@users.noreply.github.com>
2025-05-11 05:59:58 +0900

    feat: nuqs のloaderとparserを作成

---
+++ src/lib/search-params.ts
@@ -0,0 +1,30 @@
+import { categoryKeys, prefCodeSchema } from "@/types";
+import {
+	createLoader,
+	createParser,
+	parseAsArrayOf,
+	parseAsStringLiteral,
+} from "nuqs/server";
+
+const parseAsPrefCode = createParser({
+	parse(queryValue) {
+		if (Number.isNaN(Number(queryValue))) return null;
+		return prefCodeSchema.parse(Number(queryValue));
+	},
+	serialize(value) {
+		return value.toString();
+	},
+});
+
+export const populationSearchParams = {
+	prefCodes: parseAsArrayOf(parseAsPrefCode).withDefault([]).withOptions({
+		shallow: false,
+	}),
+	category: parseAsStringLiteral(categoryKeys)
+		.withDefault("total")
+		.withOptions({
+			shallow: false,
+		}),
+};
+
+export const loadSearchParams = createLoader(populationSearchParams);
