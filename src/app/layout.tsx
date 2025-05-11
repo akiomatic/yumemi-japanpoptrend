a69a9915c882063a8aac14a4b3a2b1dd619c2349
akio <124953279+akiomatic@users.noreply.github.com>
2025-05-11 09:07:57 +0900

    chore: lang属性を変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="ja">
 			<body
 				className={`${kosugiMaru.variable} font-kosugi-maru antialiased min-h-screen`}
 			>
